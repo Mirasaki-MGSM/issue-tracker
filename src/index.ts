23e4c6d92976ddc232983c403c8e512770908362
Mirasaki <pcr.hillebrand1998@gmail.com>
2025-01-30 17:07:42 +0100

    chore: use GPL-3.0-only license

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import { initDiscord } from './discord/init';
 import { parsedEnv } from './env';
 import { initGitHub } from './github/init';
+import pkg from '../package.json';
 
 /**
- * This is the entry point for an application that creates a seamless bridge/connection
- * between GitHub issues and a Discord Forum channel.
+  Copyright (C) 2025 Richard Hillebrand
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  __*THIS LICENSE APPLIES TO THE ENTIRE REPOSITORY, NOT JUST THIS FILE.*__
  */
 const main = async () => {
+  console.log(`[${pkg.name}] v${pkg.version} - ${pkg.description}`);
+  console.log(`[${pkg.name}] Author: ${pkg.author.name} <${pkg.author.email}> (${pkg.author.url})`);
+  console.log(`[${pkg.name}] License: ${pkg.license}`);
+  
   console.log(`Starting with following environment:`, Object.fromEntries(Object.entries(parsedEnv).map(([key, value]) => [
     key,
     key.includes('TOKEN') || key.includes('SECRET') ? '***' : value,
   ])));
+  
   await Promise.all([
     initDiscord(),
     initGitHub(),
